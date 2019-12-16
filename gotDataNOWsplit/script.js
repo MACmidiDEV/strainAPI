d108d38a2eb98959e11e8e34104c23a0b75affb9
MAC_DEV <mcamacho1990@icloud.com>
2019-12-16 18:54:56 -0500

    fixBuildHeaders

---
+++ gotDataNOWsplit/script.js
@@ -0,0 +1,61 @@
+const base = "https://strainapi.evanbusse.com/ofJ0JOx/"
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", base + type);
+    // xhr.open("GET", "https://strainapi.evanbusse.com/ofJ0JOx/strains/search/all");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+    // obj = this.obj;
+    // obj = obj;
+
+    Object.keys(obj).forEach(function (key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+
+function writeToDoc(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        // data = JSON.stringify(data);
+        data = this.data;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            // el.innerHTML += "<p>" + item.name + "</p>";
+        });
+
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+    });
+}
+
+// function writeToDoc(type) {
+//     getData(type, function (data) {
+//         // data = JSON.stringify(data);
+//         // document.getElementById("data").innerHTML = data; 
+//         // console.log(data);
+//         // console.log(JSON.parse(data));
+
+//         // document.getElementById("data").innerHTML = data.Afpak.race; 
+//         // document.getElementById("data1").innerHTML = data.Afpak; 
+//         // document.getElementById("data2").innerHTML = JSON.stringify(data.Afpak); 
+//         // document.getElementById("data3").innerHTML = JSON.stringify(data); 
+//         // document.getElementById("data3").innerHTML = data.Afpak.race; 
+//         // console.log(Object.keys(item).forEach(function(key){console.log(key);}))
+//     });
+// }
